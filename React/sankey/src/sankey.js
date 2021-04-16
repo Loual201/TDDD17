a1ba618c64121879fbf51fe793f98a78b142cc05
Loual201 <louise.almer97@gmail.com>
2021-04-16 16:36:18 +0200

    Lite updateringar

--- React/sankey/src/sankey.js
+++ React/sankey/src/sankey.js
@@ -27,7 +27,8 @@ const MysteriousSankey = ({ data, width, height }) => {
   const [links, setLinks] = useState([]);
   const [graph, setGraph] = useState({ nodes: [], links: [] });
   var uniqueNodes = [];
-
+  var uniquelinks = [];
+  
   useEffect(() => {
     data.map((d) => {
       setNodes((nodes) => [...nodes, { name: d.source }]);
@@ -35,15 +36,16 @@ const MysteriousSankey = ({ data, width, height }) => {
 
       setLinks((links) => [
         ...links,
-        { source: d.source, target: d.target, value: d.value },
+        { source: d.source, target: d.target, value: d.value},
       ]);
     });
   }, []);
-
+  
   // Ta ut alla unika noder
   nodes.map((n) => {
     if (!uniqueNodes.includes(n.name)) {
       uniqueNodes = [...uniqueNodes, n.name];
+      //console.log("uniqueNodes")
     }
   });
 
@@ -51,38 +53,52 @@ const MysteriousSankey = ({ data, width, height }) => {
   links.map((link) => {
     link.source = uniqueNodes.indexOf(link.source);
     link.target = uniqueNodes.indexOf(link.target);
+
   });
 
   //setGraph((g) => ({ nodes: uniqueNodes, links: [] }));
+  //console.log(uniqueNodes)
+  var nodesAsObjeccts= []
+  uniqueNodes.map((n) => {
+    nodesAsObjeccts = [...nodesAsObjeccts,{name:n}];
+  });
 
-  const { n_nodes, _n_links } = sankey()
-    .nodeWidth(15)
-    .nodePadding(10)
-    .extent([
-      [1, 1],
-      [width - 1, height - 5],
-    ])({ nodes: nodes, links: links });
+  uniquelinks = links
+  //console.log(uniqueNodes)
+  //sankey().nodes(uniqueNodes).links(uniquelinks).layout
+  
+    if(uniqueNodes.length != 0 && uniquelinks.length != 0){
+          const { n_nodes, _n_links } = sankey()
+        .nodeWidth(15)
+        .nodePadding(10)
+        //.nodeAlign('justify') TODO: Fix Left alignment
+        .extent([
+          [1, 1],
+          [width - 1, height - 5],
+        ])({ nodes: nodesAsObjeccts, links: uniquelinks });
+      }
+    
 
   //console.log('nodes');
   //console.log(nodes);
-  //const color = chroma.scale('Set3').classes(nodes.length);
-  const colorScale = d3.scaleLinear().domain([0, nodes.length]).range([0, 1]);
+  const color = chroma.scale('Set3').classes(nodesAsObjeccts.length);
+  const colorScale = d3.scaleLinear().domain([0, nodesAsObjeccts.length]).range([0, 1]);
 
   return (
     <g style={{ mixBlendMode: 'multiply' }}>
-      {/**  {nodes.map((node, i) => (
+       {nodesAsObjeccts.map((node, i) => (
         <SankeyNode
           {...node}
-          // color={color(colorScale(i)).hex()}
+         color={color(colorScale(i)).hex()}
           key={node.name}
         />
       ))}
-      {links.map((link, i) => (
+      {uniquelinks.map((link, i) => (
         <SankeyLink
           link={link}
-          // color={color(colorScale(link.source.index)).hex()}
+          color={color(colorScale(link.source.index)).hex()}
         />
-      ))}*/}
+      ))}
     </g>
   );
 };
