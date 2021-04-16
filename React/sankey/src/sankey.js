f6a6cb67f9e13e924ce6b5ed37218c73cd096cc2
Nina Argillander <nina@argillander.se>
2021-04-16 13:19:45 +0200

    react funkar lite mer nu

--- React/sankey/src/sankey.js
+++ React/sankey/src/sankey.js
@@ -22,15 +22,17 @@ const SankeyLink = ({ link, color }) => (
 );
 
 const MysteriousSankey = ({ data, width, height }) => {
-  console.log(data);
+  // console.log(data);
   const [nodes, setNodes] = useState([]);
   const [links, setLinks] = useState([]);
+  const [graph, setGraph] = useState({ nodes: [], links: [] });
+  var uniqueNodes = [];
 
   useEffect(() => {
-    console.log('use effect');
     data.map((d) => {
-      console.log('mapping data');
       setNodes((nodes) => [...nodes, { name: d.source }]);
+      setNodes((nodes) => [...nodes, { name: d.target }]);
+
       setLinks((links) => [
         ...links,
         { source: d.source, target: d.target, value: d.value },
@@ -38,8 +40,20 @@ const MysteriousSankey = ({ data, width, height }) => {
     });
   }, []);
 
-  console.log(nodes);
-  console.log(links);
+  // Ta ut alla unika noder
+  nodes.map((n) => {
+    if (!uniqueNodes.includes(n.name)) {
+      uniqueNodes = [...uniqueNodes, n.name];
+    }
+  });
+
+  // Skapa länkar mellan nodernas indexes
+  links.map((link) => {
+    link.source = uniqueNodes.indexOf(link.source);
+    link.target = uniqueNodes.indexOf(link.target);
+  });
+
+  //setGraph((g) => ({ nodes: uniqueNodes, links: [] }));
 
   const { n_nodes, _n_links } = sankey()
     .nodeWidth(15)
@@ -47,16 +61,16 @@ const MysteriousSankey = ({ data, width, height }) => {
     .extent([
       [1, 1],
       [width - 1, height - 5],
-    ])(data);
+    ])({ nodes: nodes, links: links });
 
-  console.log('nodes');
-  console.log(n_nodes);
+  //console.log('nodes');
+  //console.log(nodes);
   //const color = chroma.scale('Set3').classes(nodes.length);
   const colorScale = d3.scaleLinear().domain([0, nodes.length]).range([0, 1]);
 
   return (
     <g style={{ mixBlendMode: 'multiply' }}>
-      {nodes.map((node, i) => (
+      {/**  {nodes.map((node, i) => (
         <SankeyNode
           {...node}
           // color={color(colorScale(i)).hex()}
@@ -68,7 +82,7 @@ const MysteriousSankey = ({ data, width, height }) => {
           link={link}
           // color={color(colorScale(link.source.index)).hex()}
         />
-      ))}
+      ))}*/}
     </g>
   );
 };
